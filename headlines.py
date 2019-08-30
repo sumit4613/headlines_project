f00d14f9ff0c8298332a2be9078b5999b86b7d84
Sumit Singh <sumiterw11@gmail.com>
2019-08-30 10:48:59 +0530

    completed

--- headlines.py
+++ headlines.py
@@ -37,28 +37,19 @@ CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=25481102dd
 @app.route("/")
 def home():
     # get customized headlines, based on user input or default
-    publication = request.args.get('publication')
-    if not publication:
-        publication = request.cookies.get('publication')
-        if not publication:
-            publication = DEFAULTS['publication']
+    publication = get_value_with_fallback('publication')
     articles = get_news(publication)
 
     # get customized weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback('city')
     weather = get_weather(city)
 
     # get customized currency based on user input or default
-    currency_from = request.args.get('currency_from')
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get('currency_to')
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback('currency_from')
+    currency_to = get_value_with_fallback('currency_to')
     rate, currencies = get_rates(currency_from, currency_to)
 
+    # save cookies and return template
     response = make_response(render_template(
         'home.html',
         articles=articles,
@@ -109,6 +100,14 @@ def get_rates(frm, to):
     frm_rate = parsed.get(frm.upper())
     to_rate = parsed.get(to.upper())
     return (to_rate/frm_rate, parsed.keys())
+
+
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
 
 
 if __name__ == '__main__':
